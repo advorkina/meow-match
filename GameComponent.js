598cefba19158408f1e1d5e9447ba16af1f78878
Anastasia Dvorkina <anastasia.dvorkina@gmail.com>
2019-01-17 01:20:52 +0100

    small style fixes

--- GameComponent.js
+++ GameComponent.js
@@ -6,13 +6,13 @@ export default class GameComponent extends Component {
   cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   render() {
     return (
+      // <View style={styles.container}>
       <View style={styles.container}>
-        <View style={styles.cards}>
-          {this.cards.map((item, index) => (
-            <GameCardComponent key={index} />
-          ))}
-        </View>
+        {this.cards.map((item, index) => (
+          <GameCardComponent key={index} />
+        ))}
       </View>
+      // </View>
     );
   }
 }
@@ -20,16 +20,11 @@ export default class GameComponent extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
-  },
-  cards: {
-    flex: 0.5,
+    alignContent: 'center',
     flexWrap: 'wrap',
-    flexDirection: 'row',
-    alignItems: 'center',
-    alignContent: 'space-around',
     justifyContent: 'space-around'
   }
 });
